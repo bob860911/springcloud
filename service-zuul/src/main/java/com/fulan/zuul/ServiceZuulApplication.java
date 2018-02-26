2831677c2a7ed35b77b738f128f3fe52629aeed6
bob860911 <35831251+bob860911@users.noreply.github.com>
2018-02-26 15:38:55 +0800

    Update ServiceZuulApplication.java

--- service-zuul/src/main/java/com/fulan/zuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/fulan/zuul/ServiceZuulApplication.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages="com.fulan.zuul")
 public class ServiceZuulApplication {
 	public static void main(String[] args) {
-		System.out.println("2222222222222");
 		SpringApplication.run(ServiceZuulApplication.class, args);
 	}
 }
